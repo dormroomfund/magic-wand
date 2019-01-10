6d1af3c3ce2b8981a9d67c3e70e85c7189ddbb71
ERHSQuizbowl <vinaichand@yahoo.com>
2019-01-10 16:22:28 -0500

    Clean up promise statement and fix typo in makefile

--- server/migrations/20190109215635_add_email_to_users.js
+++ server/migrations/20190109215635_add_email_to_users.js
@@ -1,18 +1,14 @@
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('users', (table) => {
-      table
-        .text('email')
-        .notNullable()
-        .defaultTo('');
-    }),
-  ]);
+exports.up = function(knex) {
+  return knex.schema.table('users', (table) => {
+    table
+      .text('email')
+      .notNullable()
+      .defaultTo('');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('users', (table) => {
-      table.dropColumn('email');
-    }),
-  ]);
+exports.down = function(knex) {
+  return knex.schema.table('users', (table) => {
+    table.dropColumn('email');
+  });
 };
