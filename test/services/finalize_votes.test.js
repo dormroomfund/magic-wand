a4a2fe49226062c7b227a6b2c403b5ca7a0e1d13
ERHSQuizbowl <vinaichand@yahoo.com>
2019-01-13 18:58:24 -0500

    Remove test file

--- test/services/finalize_votes.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../server/app');
-
-describe("'finalize votes' service", () => {
-  it('registered the service', () => {
-    const service = app.service('finalize-votes');
-    expect(service).toBeTruthy();
-  });
-});
