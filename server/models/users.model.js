3e41dbe45f9d0ab933961fef5e6e3d557c03c52c
Vinai Rachakonda <rachakondavinai@gmail.com>
2019-01-15 13:05:05 -0500

    Models (#5)
    Add models for feathers-objection.

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,6 +1,7 @@
 // See https://vincit.github.io/objection.js/#models
 // for more of what you can do here.
 import { Model } from 'objection';
+import schema from '../../shared/schema';
 
 export default class User extends Model {
   static get tableName() {
@@ -8,12 +9,23 @@ export default class User extends Model {
   }
 
   static get jsonSchema() {
-    return {
-      type: 'object',
-      required: ['password'],
+    return schema.users;
+  }
 
-      properties: {
-        auth0Id: { type: 'string' },
+  static get relationMappings() {
+    return {
+      associated_companies: {
+        relation: Model.ManyToManyRelation,
+        modelClass: `${__dirname}/companies.model`,
+        join: {
+          from: 'users.id',
+          through: {
+            from: 'users_companies.user_id',
+            to: 'users_companies.company_id',
+          },
+          to: 'companies.id',
+          extra: ['permissions'],
+        },
       },
     };
   }
---
+++ server/models/votes.model.js
@@ -0,0 +1,33 @@
+import { Model } from 'objection';
+import schema from '../../shared/schema';
+
+export default class Vote extends Model {
+  static get tableName() {
+    return 'votes';
+  }
+
+  static get jsonSchema() {
+    return schema.votes;
+  }
+
+  static get relationMappings() {
+    return {
+      voted_company: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/companies.model`,
+        join: {
+          from: 'votes.company_id',
+          to: 'companies.id',
+        },
+      },
+      voted_user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/users.model`,
+        join: {
+          from: 'users.id',
+          to: 'votes.partner_id',
+        },
+      },
+    };
+  }
+}
