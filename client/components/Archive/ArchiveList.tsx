b3563a86d9a374ab57c8e85ca21b74f49f06d554
Casey Chow <casey.chow@gmail.com>
2019-02-15 21:02:26 -0500

    add view company button

--- client/components/Archive/ArchiveList.tsx
+++ client/components/Archive/ArchiveList.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Card from 'react-bootstrap/lib/Card';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
 import { Subscribe } from 'unstated';
 import ArchiveContainer from '../../containers/ArchiveContainer';
+import { Link } from '../../routes';
 
 export default () => {
   return (
@@ -17,21 +19,26 @@ export default () => {
                 <Card.Body>
                   <Card.Title>{company.name} </Card.Title>
                   <Card.Text>{company.description}</Card.Text>
-                  {company.archived ? (
-                    <Button
-                      variant="info"
-                      onClick={() => ac.restoreCompany(company.id)}
-                    >
-                      Restore
-                    </Button>
-                  ) : (
-                    <Button
-                      variant="danger"
-                      onClick={() => ac.archiveCompany(company.id)}
-                    >
-                      Archive
-                    </Button>
-                  )}
+                  <ButtonGroup>
+                    <Link route="company" params={{ id: company.id }}>
+                      <Button variant="primary">View</Button>
+                    </Link>
+                    {company.archived ? (
+                      <Button
+                        variant="info"
+                        onClick={() => ac.restoreCompany(company.id)}
+                      >
+                        Restore
+                      </Button>
+                    ) : (
+                      <Button
+                        variant="danger"
+                        onClick={() => ac.archiveCompany(company.id)}
+                      >
+                        Archive
+                      </Button>
+                    )}
+                  </ButtonGroup>
                 </Card.Body>
               </Card>
             ))
