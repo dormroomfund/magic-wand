d78bdaa9d0966de36dce1c478e4f3e7a807fe7c1
Casey Chow <casey.chow@gmail.com>
2019-02-17 22:59:14 -0500

    rudimentary typeform ingress (#33)
    * typeform ingress
    
    * test against typeform

--- server/services/index.ts
+++ server/services/index.ts
@@ -4,10 +4,12 @@ import votes from './votes/votes.service';
 import finalizeVotes from './votes/finalize_votes.service';
 
 import companies from './companies/companies.service';
+import companiesWebhookService from './companies/companies-webhook.service';
 
 export default (app) => {
   app.configure(users);
   app.configure(votes);
   app.configure(finalizeVotes);
   app.configure(companies);
+  app.configure(companiesWebhookService);
 };
