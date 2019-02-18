43b4ffe8ec41b3af868222d64949ffd3ad16fe76
Casey Chow <casey.chow@gmail.com>
2019-02-18 18:04:06 -0500

    add pipeline button

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import { Subscribe } from 'unstated';
 import Layout from '../components/Layout/Layout';
+import UserContainer, { AuthState } from '../containers/UserContainer';
+import { Router } from '../routes';
 
 export default () => (
   <Layout>
-    <div>Welcome to next.js!</div>
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <h1 className="mt-5 text-center">Magic Wand</h1>
+        <Subscribe to={[UserContainer]}>
+          {(uc: UserContainer) => {
+            switch (uc.authState) {
+              case AuthState.LoggedOut:
+                return (
+                  <Button block size="lg" href="/auth/auth0" className="mt-2">
+                    Log In
+                  </Button>
+                );
+              case AuthState.LoggingIn:
+                return null;
+              case AuthState.LoggedIn:
+                return (
+                  <Button
+                    onClick={() => Router.pushRoute('/pipeline')}
+                    block
+                    size="lg"
+                    className="mt-2"
+                  >
+                    Proceed to Pipeline
+                  </Button>
+                );
+            }
+          }}
+        </Subscribe>
+      </Col>
+    </Row>
   </Layout>
 );
