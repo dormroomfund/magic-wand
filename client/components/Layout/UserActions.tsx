7bf6500bbe108e636490d75907d2d4913c6e4618
Casey Chow <casey.chow@gmail.com>
2019-03-21 16:01:03 -0400

    replace user email with user profile image

--- client/components/Layout/UserActions.tsx
+++ client/components/Layout/UserActions.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/lib/Dropdown';
 import Nav from 'react-bootstrap/lib/Nav';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import NavLink from 'react-bootstrap/lib/NavLink';
+import styled from 'styled-components';
+import Image from 'react-bootstrap/lib/Image';
 import { Subscribe } from 'unstated';
 import UserContainer, { AuthState } from '../../containers/UserContainer';
 import { UnreachableCaseError } from '../../lib/errors';
-import { Link, Router } from '../../routes';
-import styled from 'styled-components';
-import Dropdown from 'react-bootstrap/lib/Dropdown';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import NavLink from 'react-bootstrap/lib/NavLink';
+import { Router } from '../../routes';
+
+const ProfileImage = styled(Image)`
+  max-height: 32px;
+`;
 
 export default () => (
   <Subscribe to={[UserContainer]}>
@@ -20,9 +24,13 @@ export default () => (
           return null;
         case AuthState.LoggedIn:
           return (
-            <Dropdown as={NavItem}>
+            <Dropdown as={NavItem} alignRight>
               <Dropdown.Toggle as={NavLink} id="navbar-auth-dropdown">
-                {uc.state.user && uc.state.user.email}
+                {uc.user && uc.user.photo ? (
+                  <ProfileImage src={uc.user.photo} roundedCircle />
+                ) : uc.user ? (
+                  uc.user.email
+                ) : null}
               </Dropdown.Toggle>
               <Dropdown.Menu>
                 <Dropdown.Item onClick={() => Router.pushRoute('/settings')}>
