5109370b5830ecb9e3dda53d73503b05db85e5e0
Casey Chow <casey.chow@gmail.com>
2019-03-23 16:51:45 -0400

    Revert "Add drive api (#42)" (#47)
    This reverts commit ad583c8e54f0beee54dd788d6ffa049a91105fc1.

--- client/schemas/app.d.ts
+++ client/schemas/app.d.ts
@@ -2,11 +2,9 @@ import { Application } from '@feathersjs/feathers';
 import { Company } from './company';
 import { User } from './user';
 import { Vote } from './vote';
-import { GoogleDriveDocument } from './gdrive';
 
 export interface ServiceTypes {
   'api/companies': Company;
-  'api/gdrive': GoogleDriveDocument;
   'api/users': User;
   'api/votes': Vote;
 }
