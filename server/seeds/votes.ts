0c3b2d2f0c69c48c7ca975f7be5e769c4cae11d1
Casey Chow <casey.chow@gmail.com>
2019-04-05 22:23:50 -0400

    set up cypress testing (#54)
    * initialize cypress
    
    * update typescript, exclude cypress
    
    * set up semaphore ci integration

--- server/seeds/votes.ts
+++ server/seeds/votes.ts
@@ -7,28 +7,28 @@ exports.seed = function(knex: Knex) {
     .then(() =>
       // Inserts seed entries
       knex('votes').insert([
-        {
-          id: 1,
-          vote_type: 'Final',
-          partner_id: 1,
-          company_id: 2,
-          market_score: 6,
-          product_score: 6,
-          team_score: 6,
-          fit_score: 6,
-          comment: 'Love the business model!',
-        },
-        {
-          id: 2,
-          vote_type: 'Final',
-          partner_id: 2,
-          company_id: 2,
-          market_score: 5,
-          product_score: 5,
-          team_score: 5,
-          fit_score: 5,
-          comment: 'Has some ethics problems but big fan!',
-        },
+        // {
+        //   id: 1,
+        //   vote_type: 'Final',
+        //   partner_id: 1,
+        //   company_id: 2,
+        //   market_score: 6,
+        //   product_score: 6,
+        //   team_score: 6,
+        //   fit_score: 6,
+        //   comment: 'Love the business model!',
+        // },
+        // {
+        //   id: 2,
+        //   vote_type: 'Final',
+        //   partner_id: 2,
+        //   company_id: 2,
+        //   market_score: 5,
+        //   product_score: 5,
+        //   team_score: 5,
+        //   fit_score: 5,
+        //   comment: 'Has some ethics problems but big fan!',
+        // },
       ])
     );
 };
