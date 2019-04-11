eaf15de96dd7374ccf37aebefdad7f27b72a8833
Casey Chow <casey.chow@gmail.com>
2019-04-10 20:37:22 -0400

    fix seeds (#62)
    * fix seeds
    
    * fix pkey
    
    * fix uniqueness

--- server/seeds/user.ts
+++ server/seeds/user.ts
@@ -9,7 +9,7 @@ export const seed = async (knex: Knex) => {
   await knex('user').insert([
     {
       id: 1,
-      auth0: '{}',
+      auth0: '{ "id": "auth0|5ca584f3ed19ed110c8cdced"}',
       auth0Id: 'auth0|5ca584f3ed19ed110c8cdced',
       permissions: 'partner',
       firstName: 'John',
@@ -22,6 +22,22 @@ export const seed = async (knex: Knex) => {
       ethnicity: Ethnicity.Asian,
       partnerTeam: Team.Philadelphia,
       partnerPosition: Position.InvestmentPartner,
+    },
+    {
+      id: 2,
+      auth0: '{ "id": "auth0|5ca584f32392319ed110c8cdced" }',
+      auth0Id: 'auth0|5ca584f322392319ed110c8cdced',
+      permissions: 'partner',
+      firstName: 'Veronica',
+      lastName: 'McDougal',
+      school: 'Yalevard',
+      email: 'magic-wand-2@mailinator.com',
+      gender: Gender.Female,
+      photo:
+        'https://s.gravatar.com/avatar/c1855f49487b41b6e92f225938ec45db?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fma.png',
+      ethnicity: Ethnicity.White,
+      partnerTeam: Team.Philadelphia,
+      partnerPosition: Position.ManagingPartner,
     },
   ]);
 };
