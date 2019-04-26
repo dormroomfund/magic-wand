d8a9369961769e84b61b490b8ecdc3ea57eefa32
Casey Chow <casey.chow@gmail.com>
2019-04-26 14:00:53 -0400

    address pr concerns

--- client/components/CompanyProfile/AttachmentDropdown.tsx
+++ client/components/CompanyProfile/AttachmentDropdown.tsx
@@ -5,44 +5,40 @@ import DropdownItem from 'react-bootstrap/lib/DropdownItem';
 import { CompanyLink } from '../../schemas/company';
 import { DocumentTypes } from '../../schemas/gdrive';
 
-const StyledDropdown = styled(Dropdown)`
-  border: none;
+const StyledDropdownToggle = styled(Dropdown.Toggle)`
+  border: 2px solid #dadde0;
+  width: auto;
+  background-color: white;
+  color: #1c37c5;
+  padding: 0.4% 0.9% 0.5% 0.9%;
+  cursor: pointer;
 
-  button {
-    border: 2px solid #dadde0;
-    width: auto;
+  &:hover {
+    background-color: #1c37c5;
+    color: white;
+    border: 2px solid #1c37c5;
+  }
+`;
+
+const StyledDropdownMenu = styled(Dropdown.Menu)`
+  width: auto;
+  padding: 0;
+  margin: 0;
+
+  a {
     background-color: white;
     color: #1c37c5;
-    padding: 0.4% 0.9% 0.5% 0.9%;
+    padding: 4% 5% 3% 5%;
     cursor: pointer;
+    font-size: 0.9rem;
 
     &:hover {
       background-color: #1c37c5;
       color: white;
-      border: 2px solid #1c37c5;
-    }
-  }
 
-  div {
-    width: auto;
-    padding: 0;
-    margin: 0;
-
-    a {
-      background-color: white;
-      color: #1c37c5;
-      padding: 4% 5% 3% 5%;
-      cursor: pointer;
-      font-size: 0.9rem;
-
-      &:hover {
+      a {
         background-color: #1c37c5;
         color: white;
-
-        a {
-          background-color: #1c37c5;
-          color: white;
-        }
       }
     }
   }
@@ -54,23 +50,20 @@ interface AttachmentDropdownProps {
 
 export default ({ links = [] }: AttachmentDropdownProps) => (
   <div>
-    <StyledDropdown>
-      <Dropdown.Toggle variant="secondary" id="dropdown-basic" size="sm">
+    <Dropdown className="border-0">
+      <StyledDropdownToggle variant="secondary" id="dropdown-basic" size="sm">
         Attachments
-      </Dropdown.Toggle>
+      </StyledDropdownToggle>
 
-      <Dropdown.Menu>
+      <StyledDropdownMenu>
         {links.map(({ name, url }) =>
           Object.values(DocumentTypes).includes(name) ? (
-            <DropdownItem key={name}>
-              <a href={url} target="_blank" rel="noopener noreferrer">
-                {' '}
-                {name}{' '}
-              </a>
+            <DropdownItem key={name} href={url} target="_blank">
+              {name}
             </DropdownItem>
           ) : null
         )}
-      </Dropdown.Menu>
-    </StyledDropdown>
+      </StyledDropdownMenu>
+    </Dropdown>
   </div>
 );
