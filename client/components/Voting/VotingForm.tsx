afdd0907b9590b34a268d5b9a2cbe86ab4569efa
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2019-11-23 14:49:37 -0500

    Clean up import

--- client/components/Voting/VotingForm.tsx
+++ client/components/Voting/VotingForm.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Form, { ISubmitEvent, IChangeEvent } from 'react-jsonschema-form-bs4';
 import Button from 'react-bootstrap/lib/Button';
 import { voteFormSchema, VoteFields } from '../../lib/voting';
-import { Vote } from '../../schemas/vote';
 
 export interface VotingFormProps {
   formData: VoteFields;
