ff620cc557ac674d450e8689225d7d02c1fa9ab1
Arun Kirubarajan <kiruba@cis.upenn.edu>
2019-12-22 22:40:10 -0500

    Rework testing infrastructure (#78)
    * Renamed .babelrc
    
    * Add ts-jest and add jest.config.js
    
    * Convert test files to typescript
    
    * Fix make command for test
    
    * Overhaul test files to true test cases
    
    * Fix semaphore typecheck error
    
    * Update jest.config.js

--- test/services/users.test.js
+++
@@ -1,8 +0,0 @@
-import { service as _service } from '../../server/app';
-
-describe("'users' service", () => {
-  it('registered the service', () => {
-    const service = _service('users');
-    expect(service).toBeTruthy();
-  });
-});
