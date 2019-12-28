c5a6bf8d433959260f2ea24b10fe1c4f09c70dd8
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2019-12-28 15:57:21 -0500

    Add metadata to company card

--- client/components/Archive/ArchiveList.tsx
+++ client/components/Archive/ArchiveList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Card from 'react-bootstrap/lib/Card';
+import ListGroup from 'react-bootstrap/lib/ListGroup';
 import { Link } from '../../routes';
 import { archivedStates, Company } from '../../schemas/company';
 
@@ -18,16 +19,36 @@ export default ({
 }: ArchiveListProps) => (
   <>
     {companies.map((company) => (
-      <Card key={company.id}>
+      <Card key={company.id} style={{ marginBottom: '1rem', width: '25rem' }}>
         <Card.Body>
           <Card.Title>{company.name}</Card.Title>
-          <Card.Text>{company.description}</Card.Text>
-          <ButtonGroup>
-            <Link route="company" params={{ id: company.id }}>
-              <Button variant="primary">View</Button>
-            </Link>
-          </ButtonGroup>
+          <Card.Text>
+            {company.description} <br />
+          </Card.Text>
         </Card.Body>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            {' '}
+            <strong>Status: </strong> {company.status}{' '}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {' '}
+            <strong>Date: </strong> {company.pitchDate}{' '}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {' '}
+            <strong>Team: </strong> {company.team}{' '}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {' '}
+            <strong>Tags: </strong> {company.industries.toString()}{' '}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Link route="company" params={{ id: company.id }}>
+              <Button variant="primary">View Page</Button>
+            </Link>
+          </ListGroup.Item>
+        </ListGroup>
       </Card>
     ))}
   </>
