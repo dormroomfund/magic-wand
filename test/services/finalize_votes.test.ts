3860caca6259251a99e8c9dbb6a4ab3fd04a0deb
VinaiRachakonda <rachakondavinai@gmail.com>
2020-01-06 13:52:47 -0500

    trash

--- test/services/finalize_votes.test.ts
+++ test/services/finalize_votes.test.ts
@@ -5,8 +5,22 @@ import assert from 'assert';
 import app from '../../server/app';
 
 describe("' api/votes/finalize ' service", () => {
-  it('registered the service', () => {
+  it('registered the final_votes service', () => {
     const service = app.service('/api/votes/finalize');
+
+    assert.ok(service, 'Registered the service');
+  });
+
+  it('registered the votes service', async () => {
+    const service = app.service('/api/votes');
+
+    const ans = await app.service('api/votes').find({
+      query: {
+        voteType: 'prevote',
+      },
+    });
+
+    console.log(ans.data);
 
     assert.ok(service, 'Registered the service');
   });
