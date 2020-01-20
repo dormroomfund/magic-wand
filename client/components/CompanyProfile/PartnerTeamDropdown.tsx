34ca9e0b667e088c6fd975615da9a95471d096e5
Kanwar Sahdra <ksingh7265@gmail.com>
2020-01-20 18:36:53 -0500

    Add ability to assign companies to different partner team on company profile page (#83)
    * Add dropdown to select team -> issue with value assignment
    
    * Use values for Teams enum, not keys
    
    * Use Object.values(Team) directly in dropdown instead of passing as prop
    
    * Remove unneeded import
    
    Co-authored-by: Vinai Rachakonda <rachakondavinai@gmail.com>

---
+++ client/components/CompanyProfile/PartnerTeamDropdown.tsx
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import Dropdown from 'react-bootstrap/lib/Dropdown';
+import styled from 'styled-components';
+import { STAC } from '../../containers/ApplicationContainer';
+import { Team } from '../../schemas/common';
+
+const StyledDropdown = styled(Dropdown)`
+  border: none;
+  margin: 0.5% 1%;
+`;
+
+interface PartnerTeamDropdownProps {
+  companyId: number;
+  currentPartnerTeam: string;
+}
+
+export default class PartnerTeamDropdown extends PureComponent<
+  PartnerTeamDropdownProps
+> {
+  renderItems = (partnerTeam, companies) => (
+    <Dropdown.Item
+      key={partnerTeam}
+      onSelect={() => {
+        if (
+          window.confirm(
+            'Are you sure you want to assign this company to a different team?'
+          )
+        ) {
+          companies.patch(this.props.companyId, { team: partnerTeam });
+        }
+      }}
+    >
+      {partnerTeam}
+    </Dropdown.Item>
+  );
+
+  render() {
+    return (
+      <STAC>
+        {(ac) => (
+          <StyledDropdown>
+            <Dropdown.Toggle variant="secondary" id="dropdown-basic" size="sm">
+              {this.props.currentPartnerTeam}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {[...Object.values(Team)].map((partnerTeam) =>
+                this.renderItems(partnerTeam, ac.companies)
+              )}
+            </Dropdown.Menu>
+          </StyledDropdown>
+        )}
+      </STAC>
+    );
+  }
+}
