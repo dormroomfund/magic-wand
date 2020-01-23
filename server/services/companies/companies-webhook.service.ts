8b91049a5fc16fb51477a3bed66436e2aed240d1
Vinai Rachakonda <rachakondavinai@gmail.com>
2020-01-23 15:31:45 -0500

    Update typeform logic (#86)
    * Update typeform logic
    
    * Fix bug

--- server/services/companies/companies-webhook.service.ts
+++ server/services/companies/companies-webhook.service.ts
@@ -1,6 +1,7 @@
 import App from '../../../client/schemas/app';
 import { refsMap, getAnswerValueFromRef } from '../../../client/lib/typeform';
 import { Company, Status } from '../../../client/schemas/company';
+import { Team } from '../../../client/schemas/common';
 
 class CompaniesWebhookService {
   private app!: App;
@@ -13,11 +14,33 @@ class CompaniesWebhookService {
     // TODO: Authenticate against Typeform.
     const payload = data.form_response;
 
+    // Override team selection if company selects random
+    const videoConferenceAnswer = getAnswerValueFromRef(
+      payload,
+      refsMap.video_conference
+    );
+    const isRemoteApplication =
+      videoConferenceAnswer === 'Via video conference';
+
+    let team;
+    if (isRemoteApplication) {
+      const choices = [
+        Team.Boston,
+        Team.Philadelphia,
+        Team.NewYork,
+        Team.SanFrancisco,
+      ];
+      const choice = Math.floor(Math.random() * 4); // returns a random integer from 0 to 3
+      team = choices[choice];
+    } else {
+      team = getAnswerValueFromRef(payload, refsMap.team);
+    }
+
     const company: Company = {
       name: getAnswerValueFromRef(payload, refsMap.name),
       description: getAnswerValueFromRef(payload, refsMap.description),
       industries: [getAnswerValueFromRef(payload, refsMap.industries)],
-      team: getAnswerValueFromRef(payload, refsMap.team),
+      team,
       status: Status.Applied,
       contactEmail: getAnswerValueFromRef(payload, refsMap.email),
       typeformData: data,
