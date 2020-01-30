25de3fade149f780c615baff00f9e2da557fc662
VinaiRachakonda <rachakondavinai@gmail.com>
2020-01-30 00:30:25 -0500

    Query limit up

--- client/components/Pipeline/Kanban.tsx
+++ client/components/Pipeline/Kanban.tsx
@@ -170,6 +170,7 @@ export default class Kanban extends PureComponent<KanbanProps, KanbanState> {
           $nin: archivedStates,
         },
         $eager: 'pointPartners',
+        $limit: 10000,
       };
 
       if (currentPartnerId !== 'ALL') {
